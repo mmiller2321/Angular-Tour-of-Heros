977132294c38756494229395a5181326cfca4b79
Micah Miller <mimiller12345@gmail.com>
2021-11-16 08:42:14 -0700

    Fixed the router-outlet issue by fixing the declarations/imports in app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent }
-  // , { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+   , { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   // { path: 'dashboard', component: DashboardComponent },
   // { path: 'detail/:id', component: HeroDetailComponent },
-  // {path: 'heroes', component: HeroesComponent }
+  // { path: 'heroes', component: HeroesComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    CommonModule
+  ],            
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,26 @@
-//import { AppRoutingModule } from './app-routing.module';
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    //AppRoutingModule,
-    AppComponent,
-    HeroesComponent,
+    AppComponent, //  <---- Must include the AppComponent which is the main component of the application.
+    HeroesComponent, 
     HeroDetailComponent,
     MessagesComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
+    RouterModule,
     FormsModule
   ],
   providers: [],
